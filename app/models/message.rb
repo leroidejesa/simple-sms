f9770248154c9053ab51376cbab43a422eb71c61
leroi <leroidejesa@gmail.com>
2015-07-21 11:25:07 -0700

    add ENV variables for security

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,13 +2,16 @@ class Message < ActiveRecord::Base
   before_create :send_message
 
 private
+#for testing:
+#twilio trial number - 13237759262
+#verified google voice number - 15623847806
 
   def send_message
     response = RestClient::Request.new(
       :method => :post,
-      :url => 'https://api.twilio.com/2010-04-01/Accounts/AC7053b45d369df1c53b689ed788fe393b/Messages.json',
-      :user => 'AC7053b45d369df1c53b689ed788fe393b',
-      :password => '207d064714476a748028c1059db43b8e',
+      :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
+      :user => ENV['TWILIO_ACCOUNT_SID'],
+      :password => ENV['TWILIO_AUTH_TOKEN'],
       :payload => { :Body => body,
                     :To => to,
                     :From => from }
